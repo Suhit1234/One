b9f3712d84c4dde08d412b19c4d8b8a5f062a1a0
Suhit1234 <Suhit3945@yahoo.co.in>
2017-10-17 22:26:38 +0530

    1

---
+++ Git Practice Two/src/SuhitOne.java
@@ -0,0 +1,14 @@
+
+public class SuhitOne {
+	
+	public void display(){
+		System.out.println("Suhit1");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SuhitOne One = new SuhitOne();
+		One.display();
+	}
+
+}
