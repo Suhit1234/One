33468b99fd72dd045ccb855dc643c93870278ed9
Suhit1234 <Suhit3945@yahoo.co.in>
2017-10-17 22:40:50 +0530

    3

--- Git Practice Two/src/SuhitOne.java
+++ Git Practice Two/src/SuhitOne.java
@@ -4,6 +4,8 @@ public class SuhitOne {
 	public void display(){
 		System.out.println("Suhit1");
 		System.out.println("Suhit2");
+		System.out.println("Suhit3");
+		System.out.println("Suhit4");
 	}
 	
 	public static void main(String[] args) {
